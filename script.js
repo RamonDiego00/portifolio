a4de00d292a3443c1731959fdcf72a8a7a584149
Ramon Diego <ramondiego857@gmail.com>
2021-10-30 01:34:27 -0300

    Botão Tema Completo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// MUDAR CONTEUDO DE SKILLS
+
 function toggle(altera) {
 
     
@@ -6,6 +8,8 @@ function toggle(altera) {
         // skills.classList.remove("desativado");
         
     }
+
+// ALTERAR CORES DE FUNDO DE SKILLS
 function backColors(altera) {
     let container = document.querySelector(`.habilidades`);
     container.classList.toggle(`${altera}Style`)
@@ -20,3 +24,60 @@ function backGray(altera) {
         }, 3000)});
 
 }
+
+// MUDAR DE TEMA
+
+window.onload=function(){
+
+    const html = document.querySelector("html")
+    const checkbox = document.querySelector("input[name=theme]")
+    
+    const getStyle = (element, style) => 
+        window
+            .getComputedStyle(element)
+            .getPropertyValue(style)
+    
+    
+    const initialColors = {
+        bg1: getStyle(html, "--bg1"),
+        bg2: getStyle(html, "--bg2"),
+        colorText: getStyle(html, "--color-text"),
+        colorHeader:getStyle(html, "--color-header"),
+        colorBlocks:getStyle(html, "--color-blocks"),
+        filterSvg:getStyle(html, "--filter-svg" )
+    }
+    
+    const whiteMode = {
+        bg1: "#b4b4b4",
+        bg2: "#cecece",
+        colorText: "#00000",
+        colorHeader: "#ffff",
+        colorBlocks: "#A8A8A8",
+        filterSvg: "null"
+    }
+    
+    const transformKey = key => 
+        "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+    
+    
+    const changeColors = (colors) => {
+        Object.keys(colors).map(key => 
+            html.style.setProperty(transformKey(key), colors[key]) 
+        )
+    }
+    
+    
+    checkbox.addEventListener("change", ({target}) => {
+        target.checked ? changeColors(whiteMode) : changeColors(initialColors)
+
+        // ICON DENTRO DE INPUT
+
+        let iconTheme = document.createElement('i')
+
+        let Butoon = document.querySelector("#switch")
+
+        Butoon.appendChild(iconTheme)
+    })
+    
+  }
+
